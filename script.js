4704830f98cfe62112c62e30306bfea71687b975
ndanu-josy <josphinendanu3@gmail.com>
2021-03-13 01:28:01 +0300

    Fix function to compute daily production from each shed

--- script.js
+++ script.js
@@ -22,15 +22,19 @@
 function shedProduction(){
     
 
-    var shedA = sheds.push(document.getElementById("amt1").value);
-    var shedB = sheds.push(document.getElementById("amt2").value);
-    var shedC = sheds.push(document.getElementById("amt3").value);
-    var shedD = sheds.push(document.getElementById("amt4").value);
+    var shedA = document.getElementById("amt1").value;
+    var shedB = document.getElementById("amt2").value;
+    var shedC = document.getElementById("amt3").value;
+    var shedD = document.getElementById("amt4").value;
 
     var totalProduction = parseInt(shedA) + parseInt(shedB) +parseInt(shedC) + parseInt(shedD);
     var dailyPro = document.getElementById("dailyPro");
 
-        dailyPro.innerHTML = "Your daily production for shed A is " + 
+        dailyPro.innerHTML = "Your daily production for shed A is " + shedA + "<br>"
+                             + "Your daily production for shed B is " + shedB + "<br>"  
+                             + "Your daily production for shed C is " + shedC+ "<br>"  
+                             + "Your daily production for shed D is " + shedD + "<br>" 
+                             + "Your total milk production in a day is  " + totalProduction;
 
     //if (shedA === 0 || shedB === 0 || shedC === 0 || shedD === 0){
         //dailyPro.innerHTML = "please fill all the fields";
